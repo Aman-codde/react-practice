d986ac0ca34318abad33e07da1fde6daa5417343
aman-codde <amandeepk221@gmail.com>
2024-11-06 13:44:51 -0800

    feat: add title for section

--- src/components/home.js
+++ src/components/home.js
@@ -1,17 +1,20 @@
 import { DATA_PORTAL} from './data_portal';
 import PortalCard from "./portal-card";
+import PortalTitle from './portal-title';
 
 export default function Home(){
     return(
-        <>
-        <section className="flex flex-wrap m-4 bg-slate-50 p-4">
-            <PortalCard  {...DATA_PORTAL[0]}/>
-            <PortalCard  {...DATA_PORTAL[1]}/>
-            <PortalCard  {...DATA_PORTAL[2]}/>
-            <PortalCard  {...DATA_PORTAL[3]}/>
-            <PortalCard  {...DATA_PORTAL[4]}/>
-            <PortalCard  {...DATA_PORTAL[5]}/>
+        <section className='bg-slate-50 py-14'>
+            <PortalTitle/>
+            <div className="flex flex-wrap m-4 p-4">
+                <PortalCard  {...DATA_PORTAL[0]}/>
+                <PortalCard  {...DATA_PORTAL[1]}/>
+                <PortalCard  {...DATA_PORTAL[2]}/>
+                <PortalCard  {...DATA_PORTAL[3]}/>
+                <PortalCard  {...DATA_PORTAL[4]}/>
+                <PortalCard  {...DATA_PORTAL[5]}/>
+            </div>
         </section>
-        </>
+       
     )
 }
---
+++ src/components/portal-title.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default class PortalTitle extends React.Component {
+    render() {
+        return (
+            <div className="prose container mx-auto text-center">
+                <h1 className="text-5xl">What is Whitebox Learning?</h1>
+                <p className="text-gray-400 text-lg">
+                    Whitebox Learning (WBL) is a classroom and online based training for developers and ML & data engineers.
+                </p>
+            </div>
+        )
+    }
+}
